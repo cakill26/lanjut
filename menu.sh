2e31e20b9fe5bc4497cc759f25b017eab0735e22
envy26 <113749002+cakill26@users.noreply.github.com>
2023-03-14 14:40:18 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,6 +3,7 @@
 GitUser="cakill26"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
 #Domain
 domain=$(cat /usr/local/etc/xray/domain)
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10)
@@ -12,9 +13,9 @@ IPVPS=$(curl -s ipinfo.io/ip)
 cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
 cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
 freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
-#tram=$(free -m | awk 'NR==2 {print $2}')
-#uram=$(free -m | awk 'NR==2 {print $3}')
-#fram=$(free -m | awk 'NR==2 {print $4}')
+tram=$(free -m | awk 'NR==2 {print $2}')
+uram=$(free -m | awk 'NR==2 {print $3}')
+fram=$(free -m | awk 'NR==2 {print $4}')
 clear
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
@@ -40,28 +41,35 @@ ver=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.co
 clear
 # CEK UPDATE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info1="${Green_font_prefix}$version${Font_color_suffix}"
+Info1="${Green_font_prefix}($version)${Font_color_suffix}"
 Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
 Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
 version=$(cat /home/ver)
 new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
 #Status Version
 if [ $version = $new_version ]; then
-stl="${Info1}"
+stl="${Info2}"
 else
 stl="${Error}"
 fi
 clear
 # Getting CPU Information
-#cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-#cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
-#cpu_usage+=" %"
-load_cpu=$(printf '%-3s' "$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')")
-daily_usage=$(vnstat -d --oneline | awk -F\; '{print $6}' | sed 's/ //')
-monthly_usage=$(vnstat -m --oneline | awk -F\; '{print $11}' | sed 's/ //')
-ram_used=$(free -m | grep Mem: | awk '{print $3}')
-total_ram=$(free -m | grep Mem: | awk '{print $2}')
-ram_usage=$(echo "scale=2; ($ram_used / $total_ram) * 100" | bc | cut -d. -f1)
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
+cpu_usage+=" %"
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+
 # STATUS EXPIRED ACTIVE
 red_font_prefix="\033[32m" && blue_font_prefix="\033[31m" && blue_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}(Active)${Font_color_suffix}"
@@ -76,18 +84,6 @@ else
 fi
 echo -e "\e[32mloading...\e[0m"
 clear
-bold=$(tput bold)
-normal=$(tput sgr0)
-# // Export Color & Information
-export WH='\033[1;37m'                   
-export RED='\033[0;31m'
-export GRN='\033[0;32m'
-export YLW='\033[0;33m'
-export BLU='\033[0;34m'
-export PLE='\033[0;35m'
-export CYA='\033[0;36m'
-export LHT='\033[0;37m'
-export NC='\033[0m'
 # CERTIFICATE STATUS
 d1=$(date -d "$valid" +%s)
 d2=$(date -d "$today" +%s)
@@ -125,68 +121,82 @@ banner=$(cat /usr/bin/bannerku)
 ascii=$(cat /usr/bin/test)
 clear
 echo -e "\e[$banner_colour"
-figlet -f $ascii "$banner" | lolcat
-echo -e " \e[$line ┌──────────────────────────────────────────────────────┐${NC}"
-echo -e " \e[$line │\e[$box	       ───[ Server Information ]───              \e[$line│${NC}"
-echo -e " \e[$line └──────────────────────────────────────────────────────┘${NC}"
-echo -e " \e[$text  OPERATING SYSTEM       : ${NC}$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)${NC}"
-echo -e " \e[$text  KERNEL	          : ${NC}$(uname -r)${NC}"
-echo -e " \e[$text  SYSTEM UPTIME          : ${NC}$uptime${NC}"
-echo -e " \e[$text  IP ADDRESS		  : ${NC}$IPVPS${NC}"
-echo -e " \e[$text  DOMAIN NAME	          : ${PLE}$domain${NC}"
-echo -e " \e[$text  PROVIDED BY		  : ${YLW}$creditt${NC}"
-echo -e " \e[$text  STATUS UPDATE	  : ${NC}$stl${NC}"
-echo -e " \e[$text  CPU USAGE		  : ${NC}$load_cpu${NC}"
-echo -e " \e[$text  MEMORY USAGE           : ${NC}${ram_used}MB/${total_ram}MB (${ram_usage}%)${NC}"
-echo -e " \e[$text  BANDWIDTH DATA USAGE	  : ${WH}$daily_usage Daily/${YLW}$monthly_usage Monthly${NC}"
-echo -e " \e[$line ┌──────────────────────────────────────────────────────┐${NC}"
-echo -e "   Ssh/Ovpn  V2ray  Vless  Vlessxtls   Troj-Ws   Troj-Tls${NC}"
-echo -e "      \e[$text $total_ssh       $vmess      $vless        $xtls          $trws         $trtls${NC}"
-echo -e " \e[$line └──────────────────────────────────────────────────────┘${NC}"	
-echo -e " \e[$line ┌──────────────────────────────────────────────────────┐${NC}"
-echo -e " \e[$line │\e[$box                 ───[ SERVER MENU ]───                \e[$line│${NC}"
-echo -e " \e[$line └──────────────────────────────────────────────────────┘${NC}"
-echo -e "  "
-echo -e "    [\e[$number 01${NC}]\e[$below • Xray Vmess & Vless ${NC}      [\e[$number 07${NC}]\e[$below • Reboot ${NC}"
-echo -e "    [\e[$number 02${NC}]\e[$below • Trojan Xray & WS ${NC}        [\e[$number 08${NC}]\e[$below • Themes ${NC}"
-echo -e "    [\e[$number 03${NC}]\e[$below • SSH & OPENVPN  ${NC}          [\e[$number 09${NC}]\e[$below • Add IP ${NC}"
-echo -e "    [\e[$number 04${NC}]\e[$below • System Menu ${NC}             [\e[$number 10${NC}]\e[$below • Info ${NC}"
-echo -e "    [\e[$number 05${NC}]\e[$below • Check Running ${NC}           [\e[$number 11${NC}]\e[$below • Log Cleaner ${NC}"
-echo -e "    [\e[$number 06${NC}]\e[$below • Change Port ${NC}             [\e[$number 12${NC}]\e[$below • Exit ${NC}"
-echo -e " "
-echo -e " \e[$line ┌──────────────────────────────────────────────────────┐${NC}"
-echo -e " \e[$line │\e[$box   •••───[ Moded Script By JsPhantom @ 2023 ]───•••   \e[$line│${NC}"
-echo -e " \e[$line └──────────────────────────────────────────────────────┘${NC}"
-echo -e "                [Ctrl + C] Exit From Script" | lolcat
+figlet -f $ascii "$banner"
+echo -e "\e[$text PREMIUM-SERVER-SCRIPT-BY-DRGVPN"
+echo -e " \e[$line**************************************************************\e[m"
+echo -e "  \e[$text Cpu Model            :$cname"
+echo -e "  \e[$text Cpu Frequency        :$freq MHz"
+echo -e "  \e[$text Number Of Core       : $cores" 
+echo -e "  \e[$text Operating System     : $(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)"
+echo -e "  \e[$text Kernel               : $(uname -r)"
+echo -e "  \e[$text Total Amount Of Ram  : $tram MB"
+echo -e "  \e[$text System Uptime        : $uptime"
+echo -e "  \e[$text Ip Vps/Address       : $IPVPS"
+echo -e "  \e[$text Domain Name          : $domain\e[0m"
+echo -e "  \e[$text Order ID             : $oid"
+echo -e "  \e[$text Expired Status       : $exp $sts"
+echo -e "  \e[$text Provided By          : $creditt"
+echo -e "  \e[$text Status Update        : $stl"
+echo -e "  \e[$line***************************************************************\e[m"
+echo -e "   \e[$text CPU Usage : $cpu_usage"                                       
+echo -e "   \e[$text Used RAM  : $uram MB"                                         
+echo -e "   \e[$text Free RAM  : $fram MB"                                        
+echo -e " \e[$line***************************************************************\e[m"
+echo -e "  \e[$text Traffic\e[0m       \e[${text}Today      Yesterday     Month   "
+echo -e "  \e[$text Download\e[0m   \e[${text}   $dtoday    $dyest       $dmon   \e[0m"
+echo -e "  \e[$text Upload\e[0m     \e[${text}   $utoday    $uyest       $umon   \e[0m"
+echo -e "  \e[$text Total\e[0m       \e[${text}  $ttoday    $tyest       $tmon  \e[0m "
+echo -e " \e[$line***************************************************************\e[m"
+echo -e " \e[$text Ssh/Ovpn   V2ray   Vless   Vlessxtls   Trojan-Ws   Trojan-Tls \e[0m "    
+echo -e " \e[$below    $total_ssh         $vmess       $vless       $xtls             $trws          $trtls \e[0m "
+echo -e " \e[$line***************************************************************\e[m"
+echo -e " \e[$number  [•1]\e[m \e[$below •XRAY VMESS & VLESS\e[m"          
+echo -e "  \e[$number [•2]\e[m \e[$below •TROJAN XRAY & WS\e[m"             
+echo -e "  \e[$number [•3]\e[m \e[$below •SSH & OPENVPN\e[m"                
+echo -e "  \e[$number [•4]\e[m \e[$below •SYSTEM MENU\e[m"                  
+echo -e "  \e[$number [•5]\e[m \e[$below •CHECK RUNNING\e[m"                
+echo -e "  \e[$number [•6]\e[m \e[$below •CHANGE PORT\e[m"                  
+echo -e "  \e[$number [•7]\e[m \e[$below •REBOOT NOW VPS\e[m"
+echo -e "  \e[$number [•8]\e[m \e[$below •CHANGE THEMES\e[m"
+echo -e "  \e[$number [•9]\e[m \e[$below •ADD IP [admin only]\e[m"
+echo -e "  \e[$number [•10]\e[m \e[$below•INFO\e[m"
+echo -e "  \e[$number [•11]\e[m \e[$below•CLEAR LOG\e[m"
+echo -e "  \e[$number [•12]\e[m \e[$below•EXIT\e[m"
+echo -e " \e[$line*************************************************************\e[m"
+echo -e "  \e[$below Autoscript By        : DRGVPN"
+echo -e "  \e[$below remode by            : @info_android9"
+echo -e "  \e[$below creadit to OWNER     : PAKYAVPN && V-CODE"
+echo -e " \e[$line*************************************************************\e[m"
+echo -e "  \e[$below[Ctrl + C] keluar-dari-script\e[m"
 echo -e "\e[$below "
 read -p " Select menu :  " menu
 echo -e ""
 case $menu in
-1 | 01 )
+1)
     xraay
     ;;
-2 | 02 )
+2)
     trojaan
     ;;
-3 | 03 )
+3)
     ssh
     ;;
-4 | 04 )
+4)
     system
     ;;
-5 | 05)
+5)
     check-sc
     ;;
-6 | 06 )
+6)
     change-port
     ;;
-7 | 07 )
+7)
     reboot
     ;;
-8 | 08 )
+8)
     themes
     ;;
-9 | 09 )
+9)
     addip
     ;;
 10)
